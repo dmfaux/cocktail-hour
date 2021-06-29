6d25414940f5490b8c0cc25b8ac7821db480ca91
David Faux <david.faux@aax.co.za>
2021-06-29 19:00:55 +0200

    Added initial routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { Header, Footer } from "./components"
 import { Layout, RouterWindow} from "./App.styles";
 import GlobalStyle from "./GlobalStyle";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages";
 
 const App = () => {
   return (
-    <Layout>
-      <Header />
-      <RouterWindow>
-        Routing stuff here
-      </RouterWindow>
-      <Footer />
-      <GlobalStyle />
-    </Layout>
+    <Router>
+      <Layout>
+        <Header />
+        <RouterWindow>
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+          </Switch>
+        </RouterWindow>
+        <Footer />
+        <GlobalStyle />
+      </Layout>
+    </Router>
   );
 }
 
